c03ef5df08e3c29002651d480d14a19a67dfb2ac
Hoang <hoangg9009@gmail.com>
2025-03-04 14:35:15 +0700

    Swagger Config

--- BE/backend/src/main/java/com/glowcorner/backend/config/SwaggerConfig.java
+++ BE/backend/src/main/java/com/glowcorner/backend/config/SwaggerConfig.java
@@ -1,4 +1,28 @@
 package com.glowcorner.backend.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Glow Corner API")
+                        .version("1.0")
+                        .description("API documentation for the Skin Care Shopping application")
+                        .contact(new Contact()
+                                .name("Your Name")
+                                .email("your.email@example.com")
+                                .url("https://yourwebsite.com"))
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://mit-license.org")));
+    }
 }
