5634b45f9224c96dba92089ce8549c6fae111c2d
Hoang <hoangg9009@gmail.com>
2025-03-11 14:36:23 +0700

    update security
    update security

---
+++ BE/backend/src/main/java/com/glowcorner/backend/security/CustomUserDetailService.java
@@ -0,0 +1,29 @@
+package com.glowcorner.backend.security;
+
+import com.glowcorner.backend.model.mongoDB.User;
+import com.glowcorner.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new org.springframework.security.core.userdetails.User(email, user.getEmail(),
+                Collections.singletonList(new SimpleGrantedAuthority(user.getRole().name())));
+    }
+}
