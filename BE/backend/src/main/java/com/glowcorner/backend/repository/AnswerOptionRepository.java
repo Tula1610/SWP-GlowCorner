5f836afed8e6a8e3b6024311d7157b8aa37598c6
HMcPhong <167400827+HMcPhong@users.noreply.github.com>
2025-04-01 12:41:17 +0700

    Add AnswerOption api

--- BE/backend/src/main/java/com/glowcorner/backend/repository/AnswerOptionRepository.java
+++ BE/backend/src/main/java/com/glowcorner/backend/repository/AnswerOptionRepository.java
@@ -4,10 +4,14 @@ import com.glowcorner.backend.entity.mongoDB.AnswerOption;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AnswerOptionRepository extends MongoRepository<AnswerOption, String> {
-    Optional<AnswerOption> findAnswerOptionByOptionID(String optionID);
     void deleteAllByQuestionId(String questionID);
+
+    Optional<AnswerOption> findAnswerOptionByOptionID(String optionID);
+    List<AnswerOption> findAnswerOptionsByQuestionId(String questionID);
+
 }
