87025f8d98e1b1a7ba58beb6d21e76dc86c3ff6e
Hoang <hoangg9009@gmail.com>
2025-04-01 20:54:52 +0700

    update feedback
    update feedback

--- BE/backend/src/main/java/com/glowcorner/backend/entity/mongoDB/Feedback.java
+++ BE/backend/src/main/java/com/glowcorner/backend/entity/mongoDB/Feedback.java
@@ -1,27 +1,40 @@
 package com.glowcorner.backend.entity.mongoDB;
 
+
+import com.glowcorner.backend.enums.Status.FeedbackStatus;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+
 import java.time.LocalDate;
+
 
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Document(collection = "feedback")
 public class Feedback {
 
-    @Id
-    String id;
 
-    String feedbackID;
+   @Id
+   String id;
 
-    String customerID; // Refers to the userID in the User collection (foreign key in SQL)
 
-    int rating; // Rating is represented as an integer
+   String feedbackID;
 
-    String comment; // Comment is optional and maps to NVARCHAR(255)
 
-    LocalDate feedbackDate; // Maps to the SQL DATE type
+   String customerID; // Refers to the userID in the User collection (foreign key in SQL)
+
+
+   int rating; // Rating is represented as an integer
+
+
+   String comment; // Comment is optional and maps to NVARCHAR(255)
+
+
+   LocalDate feedbackDate; // Maps to the SQL DATE type
+
+
+   FeedbackStatus status;
 }
--- BE/backend/src/main/java/com/glowcorner/backend/service/implement/FeedbackServiceImp.java
+++ BE/backend/src/main/java/com/glowcorner/backend/service/implement/FeedbackServiceImp.java
@@ -1,6 +1,8 @@
 package com.glowcorner.backend.service.implement;
 
+
 import com.glowcorner.backend.entity.mongoDB.Feedback;
+import com.glowcorner.backend.enums.Status.FeedbackStatus;
 import com.glowcorner.backend.model.DTO.FeedbackDTO;
 import com.glowcorner.backend.model.DTO.request.Feedback.CreateFeedbackRequest;
 import com.glowcorner.backend.model.mapper.CreateMapper.Feedback.CreateFeedbackRequestMapper;
@@ -9,66 +11,83 @@ import com.glowcorner.backend.repository.FeedbackRepository;
 import com.glowcorner.backend.service.interfaces.FeedbackService;
 import org.springframework.stereotype.Service;
 
+
 import java.util.List;
 import java.util.stream.Collectors;
+
 
 @Service
 public class FeedbackServiceImp implements FeedbackService {
 
-    private final FeedbackRepository feedbackRepository;
-    private final FeedbackMapper feedbackMapper;
-    private final CreateFeedbackRequestMapper createFeedbackRequestMapper;
 
-    public FeedbackServiceImp(FeedbackRepository feedbackRepository, FeedbackMapper feedbackMapper, CreateFeedbackRequestMapper createFeedbackRequestMapper) {
-        this.feedbackRepository = feedbackRepository;
-        this.feedbackMapper = feedbackMapper;
-        this.createFeedbackRequestMapper = createFeedbackRequestMapper;
-    }
+   private final FeedbackRepository feedbackRepository;
+   private final FeedbackMapper feedbackMapper;
+   private final CreateFeedbackRequestMapper createFeedbackRequestMapper;
 
-    @Override
-    public List<FeedbackDTO> getAllFeedbacks() {
-        return feedbackRepository.findAll().stream()
-                .map(feedbackMapper::toDTO)
-                .collect(Collectors.toList());
-    }
 
-    @Override
-    public FeedbackDTO getFeedbackById(String id) {
-        return feedbackRepository.findFeedbackByFeedbackID(id)
-                .map(feedbackMapper::toDTO)
-                .orElse(null);
-    }
+   public FeedbackServiceImp(FeedbackRepository feedbackRepository, FeedbackMapper feedbackMapper, CreateFeedbackRequestMapper createFeedbackRequestMapper) {
+       this.feedbackRepository = feedbackRepository;
+       this.feedbackMapper = feedbackMapper;
+       this.createFeedbackRequestMapper = createFeedbackRequestMapper;
+   }
 
-    @Override
-    public FeedbackDTO createFeedback(CreateFeedbackRequest request) {
-        return feedbackMapper.toDTO(feedbackRepository.save(createFeedbackRequestMapper.fromCreateRequest(request)));
-    }
 
-    @Override
-    public FeedbackDTO updateFeedback(String id, FeedbackDTO feedbackDTO) {
-        try {
-            // Find existing feedback
-            Feedback existingFeedback = feedbackRepository.findFeedbackByFeedbackID(id)
-                    .orElseThrow(() -> new RuntimeException("Feedback not found"));
+   @Override
+   public List<FeedbackDTO> getAllFeedbacks() {
+       return feedbackRepository.findAll().stream()
+               .map(feedbackMapper::toDTO)
+               .collect(Collectors.toList());
+   }
 
-            // Update
-            if (feedbackDTO.getCustomerID() != null && !feedbackDTO.getCustomerID().isEmpty()) existingFeedback.setCustomerID(feedbackDTO.getCustomerID());
-            if (feedbackDTO.getRating() != 0) existingFeedback.setRating(feedbackDTO.getRating());
-            if (feedbackDTO.getComment() != null) existingFeedback.setComment(feedbackDTO.getComment());
-            if (feedbackDTO.getFeedbackDate() != null) existingFeedback.setFeedbackDate(feedbackDTO.getFeedbackDate());
 
-            // Save update
-            Feedback updatedFeedback = feedbackRepository.save(existingFeedback);
+   @Override
+   public FeedbackDTO getFeedbackById(String id) {
+       return feedbackRepository.findFeedbackByFeedbackID(id)
+               .map(feedbackMapper::toDTO)
+               .orElse(null);
+   }
 
-            // Convert updated feedback entity to DTO
-            return feedbackMapper.toDTO(updatedFeedback);
-        } catch (Exception e) {
-            throw new RuntimeException("Fail to update feedback: " + e.getMessage(), e);
-        }
-    }
 
-    @Override
-    public void deleteFeedback(String id) {
-        feedbackRepository.deleteFeedbackByFeedbackID(id);
-    }
+   @Override
+   public FeedbackDTO createFeedback(CreateFeedbackRequest request) {
+       Feedback feedback = createFeedbackRequestMapper.fromCreateRequest(request);
+       feedback.setStatus(FeedbackStatus.PENDING);
+       return feedbackMapper.toDTO(feedbackRepository.save(feedback));
+   }
+
+
+   @Override
+   public FeedbackDTO updateFeedback(String id, FeedbackDTO feedbackDTO) {
+       try {
+           // Find existing feedback
+           Feedback existingFeedback = feedbackRepository.findFeedbackByFeedbackID(id)
+                   .orElseThrow(() -> new RuntimeException("Feedback not found"));
+
+
+           // Update
+           if (feedbackDTO.getCustomerID() != null && !feedbackDTO.getCustomerID().isEmpty()) existingFeedback.setCustomerID(feedbackDTO.getCustomerID());
+           if (feedbackDTO.getRating() != 0) existingFeedback.setRating(feedbackDTO.getRating());
+           if (feedbackDTO.getComment() != null) existingFeedback.setComment(feedbackDTO.getComment());
+           if (feedbackDTO.getFeedbackDate() != null) existingFeedback.setFeedbackDate(feedbackDTO.getFeedbackDate());
+
+
+           // Save update
+           Feedback updatedFeedback = feedbackRepository.save(existingFeedback);
+
+
+           // Convert updated feedback entity to DTO
+           return feedbackMapper.toDTO(updatedFeedback);
+       } catch (Exception e) {
+           throw new RuntimeException("Fail to update feedback: " + e.getMessage(), e);
+       }
+   }
+
+
+   @Override
+   public void deleteFeedback(String id) {
+       Feedback existingFeedback = feedbackRepository.findFeedbackByFeedbackID(id)
+               .orElseThrow(() -> new RuntimeException("Feedback not found"));
+       existingFeedback.setStatus(FeedbackStatus.DISABLE);
+       feedbackRepository.save(existingFeedback);
+   }
 }
