dacbb68a3664973aac1e0f994231cdb8a5a35af1
Tula1610 <nhoxvip114.optimusprime@gmail.com>
2025-04-07 22:17:52 +0700

    update code

--- BE/backend/src/main/java/com/glowcorner/backend/security/CustomFilterSecurity.java
+++ BE/backend/src/main/java/com/glowcorner/backend/security/CustomFilterSecurity.java
@@ -61,11 +61,12 @@ public class CustomFilterSecurity {
                 "/api/products/filter",
                 "/api/feedbacks",
                 "/api/quizzes",
+                "/api/user/**",
         };
 
 //        String[] showUrls = {"/api/manager/users/**","/api/products/**","/api/orders/**","/api/cart/**","/api/categories","/api/skin-care-routines/**","/api/promotions","/api/feedbacks/**","/api/quizzes/**"};
-        String[] showUrls = {"/api/user/**","/api/orders/customer/**","/api/cart/**","/api/categories","/api/skin-care-routines/user/**","/api/skin-care-routines/skinType/**"};
-        String[] updateUrls = {"/api/user/**","/api/cart/**","/api/skin-care-routines/**","/api/orders/staff/**"};
+        String[] showUrls = {"/api/orders/customer/**","/api/cart/**","/api/categories","/api/skin-care-routines/user/**","/api/skin-care-routines/skinType/**"};
+        String[] updateUrls = {"/api/cart/**","/api/skin-care-routines/**","/api/orders/staff/**"};
         String[] postUrls = {"/api/cart/**","/api/orders/**"};
         String[] postStaffUrls = {"/api/skin-care-routines"};
         String[] deleteUrls = {"/api/cart/**"};
