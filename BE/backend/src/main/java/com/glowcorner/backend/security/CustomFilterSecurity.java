683c3667badb559041bde75c458df1a9740efcbf
tri <tricnaSE173284@fpt.edu.vn>
2025-06-22 20:48:43 +0700

    add Url mobile

--- BE/backend/src/main/java/com/glowcorner/backend/security/CustomFilterSecurity.java
+++ BE/backend/src/main/java/com/glowcorner/backend/security/CustomFilterSecurity.java
@@ -155,7 +155,7 @@ public class CustomFilterSecurity {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://10.0.2.2:3000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
